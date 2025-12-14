a2803d7c35219480a0fc59f1155e25c279a3bd69
Andrew <andrei.dc2008@yahoo.com>
2025-12-13 20:50:00 -0500

    Wrote method generate random score and to string

--- src/main/java/org/andrew/Assignment.java
+++ src/main/java/org/andrew/Assignment.java
@@ -1,6 +1,7 @@
 package org.andrew;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class Assignment {
     private String assignmentId;
@@ -34,5 +35,37 @@ public class Assignment {
         return count == 0 ? 0 : (double) sum / count;
     }
 
+    /**
+     * Generates random score
+     */
+    public void generateRandomScore() {
+        Random rand = new Random();
 
+        for (int i = 0; i < scores.size(); i++) {
+            int r = rand.nextInt(11); // [0,10]
+            int score;
+
+            if (r == 0)
+                score = rand.nextInt(61);              // [0,60]
+            else if (r <= 2)
+                score = 60 + rand.nextInt(11);         // [60,70]
+            else if (r <= 4)
+                score = 70 + rand.nextInt(11);         // [70,80]
+            else if (r <= 8)
+                score = 80 + rand.nextInt(11);         // [80,90]
+            else
+                score = 90 + rand.nextInt(11);         // [90,100]
+
+            scores.set(i, score);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Assignment{" +
+                "assignmentId='" + assignmentId + '\'' +
+                ", assignmentName='" + assignmentName + '\'' +
+                ", weight=" + weight +
+                '}';
+    }
 }
